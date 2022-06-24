ac3878c8975f5b1cb1cedca1565648f1a45f0e02
your name <88249568+Litsavkina@users.noreply.github.com>
2022-06-24 18:40:36 -0400

    US12_AC1_Sc4 started

---
+++ src/test/java/com/cydeo/test/marina/US12_AC1_Sc4.java
@@ -0,0 +1,13 @@
+package com.cydeo.test.marina;
+
+public class US12_AC1_Sc4 {
+    /*
+
+AC:"4.Verify that truck driver should be able to Edit or delete Vehicle services log
+Truck driver  is able to an edit existing  service log
+GIVEN I am a truck driver on Vehicle Services Logs web page ,
+WHEN I click on any servise,
+THEN I am able to edit existing services.
+
+     */
+}
