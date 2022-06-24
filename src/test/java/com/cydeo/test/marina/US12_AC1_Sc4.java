c816226a7b86c53d57a1d6d200936edfd02da5de
your name <88249568+Litsavkina@users.noreply.github.com>
2022-06-24 18:53:38 -0400

    US12_AC1_Sc4 method created

--- src/test/java/com/cydeo/test/marina/US12_AC1_Sc4.java
+++ src/test/java/com/cydeo/test/marina/US12_AC1_Sc4.java
@@ -1,5 +1,7 @@
 package com.cydeo.test.marina;
 
+import org.testng.annotations.Test;
+
 public class US12_AC1_Sc4 {
     /*
 
@@ -8,6 +10,14 @@ Truck driver  is able to an edit existing  service log
 GIVEN I am a truck driver on Vehicle Services Logs web page ,
 WHEN I click on any servise,
 THEN I am able to edit existing services.
-
      */
+
+
+    @Test
+    public void user_edit_existing_service_log(){
+
+
+
+
+    }
 }
