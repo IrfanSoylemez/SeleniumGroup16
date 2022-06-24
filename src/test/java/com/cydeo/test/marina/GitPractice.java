5960b4ddc55cfa74c88a39a1090d97c99b13fe1c
your name <88249568+Litsavkina@users.noreply.github.com>
2022-06-23 22:58:12 -0400

    Hello Test

---
+++ src/test/java/com/cydeo/test/marina/GitPractice.java
@@ -0,0 +1,14 @@
+package com.cydeo.test.marina;
+
+import org.testng.annotations.Test;
+
+public class GitPractice {
+    @Test
+    public void gitPractice(){
+        System.out.println("Hello Test");
+
+        // work 1
+
+    }
+
+}
